e009bdb49d4dfb177e2ec249f83e8a79b3e50dfe
Alexy <alexy.traore@ynov.com>
2025-04-07 15:01:12 +0200

    added Date.java

--- Date.java
+++ Date.java
@@ -1,3 +1,47 @@
 public class Date {
-    
+    private int jour;
+    private int mois;
+    private int annee;
+
+    public Date() {
+        this.jour = 7;
+        this.mois = 4;
+        this.annee = 2025;
+    }
+
+    public Date(int jour, int mois, int annee) {
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+    }
+
+    public int getJour() {
+        return jour;
+    }
+
+    public void setJour(int jour) {
+        this.jour = jour;
+    }
+
+    public int getMois() {
+        return mois;
+    }
+
+    public void setMois(int mois) {
+        this.mois = mois;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    @Override
+    public String toString() {
+        return jour + "/" + mois + "/" + annee;
+    }
 }
+
